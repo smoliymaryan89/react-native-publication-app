ce30845b67a746364df60f20699418044559ab73
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-16 20:09:19 +0300

    feat: created posts screen

---
+++ src/screens/PostsScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const PostsScreen = () => {
+  return (
+    <View>
+      <Text>PostsScreen</Text>
+    </View>
+  );
+};
+
+export default PostsScreen;
+
+const styles = StyleSheet.create({});
