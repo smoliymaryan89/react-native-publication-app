fc1544f8a2fce5475606af2b2fccbee7792c069c
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-16 17:56:47 +0300

    feat: added Regular font using expo-fonts

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import LoginScreen from "./src/screens/LoginScreen";
 
-export default function App() {
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    "Roboto-regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <LoginScreen />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
+
+export default App;
