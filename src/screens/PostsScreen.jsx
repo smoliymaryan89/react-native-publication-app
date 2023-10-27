6d62fd70a9eaebd640e2637c0af7c04a0fc1be38
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-10-27 12:34:49 +0300

    feat: created post screen

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,13 +1,35 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 const PostsScreen = () => {
   return (
-    <View>
-      <Text>PostsScreen</Text>
+    <View style={styles.wrapper}>
+      <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+        <Image source={require("../assets/images/userAvatar.png")} />
+        <View>
+          <Text style={styles.name}>Natali Romanova</Text>
+          <Text style={styles.email}>email@example.com</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
-export default PostsScreen;
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 32,
+  },
+  name: {
+    color: "#212121",
+    fontSize: 13,
+    fontWeight: "700",
+  },
+  email: {
+    color: "rgba(33, 33, 33, 0.8)",
+    fontSize: 11,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default PostsScreen;
