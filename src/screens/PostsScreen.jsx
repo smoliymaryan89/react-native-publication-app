9bc8d0c6ffa5659ce2aeb464819d689b8b49c6b2
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-10-30 21:20:38 +0200

    feat: created posts screen

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,16 +1,87 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { TouchableOpacity } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Image } from "react-native";
 
 const PostsScreen = () => {
+  const { params } = useRoute();
+  const { navigate } = useNavigation();
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    if (params) {
+      setPost((prevState) =>
+        prevState.includes(params) ? prevState : [...prevState, params]
+      );
+    }
+  }, [params]);
+
   return (
     <View style={styles.wrapper}>
-      <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 8,
+          marginBottom: 32,
+        }}
+      >
         <Image source={require("../assets/images/userAvatar.png")} />
         <View>
           <Text style={styles.name}>Natali Romanova</Text>
           <Text style={styles.email}>email@example.com</Text>
         </View>
       </View>
+
+      <FlatList
+        data={post}
+        renderItem={({ item: { image, imageName, place, location } }) => (
+          <View
+            style={{
+              marginBottom: 32,
+              flex: 1,
+            }}
+          >
+            <Image source={{ uri: image }} style={styles.postImg} />
+            <Text style={styles.imgName}>{imageName}</Text>
+            <View style={styles.postContent}>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 6,
+                }}
+                onPress={() => navigate("Коментарі", { image })}
+              >
+                <Feather name="message-circle" size={24} color="#BDBDBD" />
+                <Text style={{ fontSize: 16, color: "#BDBDBD" }}>0</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={{ flexDirection: "row", gap: 4, alignItems: "center" }}
+                onPress={() => navigate("Карта", { location })}
+              >
+                <Feather name="map-pin" size={24} color="#BDBDBD" />
+                <Text
+                  style={{
+                    color: "#212121",
+                    fontSize: 16,
+                    textDecorationLine: "underline",
+                  }}
+                >
+                  {place}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+        keyExtractor={({ image }) => image}
+      />
     </View>
   );
 };
@@ -20,6 +91,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
     paddingTop: 32,
+    backgroundColor: "#fff",
   },
   name: {
     color: "#212121",
@@ -29,6 +101,22 @@ const styles = StyleSheet.create({
   email: {
     color: "rgba(33, 33, 33, 0.8)",
     fontSize: 11,
+  },
+  postContent: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  postImg: {
+    width: "100%",
+    height: 240,
+    marginBottom: 8,
+    borderRadius: 8,
+  },
+  imgName: {
+    color: "#212121",
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 8,
   },
 });
 
